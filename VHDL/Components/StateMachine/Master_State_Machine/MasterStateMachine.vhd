67629d74143369c2918a9fb81398275255f2f6ff
Doug Gilliland <doug@douglasgilliland.com>
2019-05-13 14:07:23 -0400

    Update MasterStateMachine.vhd

--- VHDL/Components/StateMachine/Master_State_Machine/MasterStateMachine.vhd
+++ VHDL/Components/StateMachine/Master_State_Machine/MasterStateMachine.vhd
@@ -8,24 +8,22 @@ ENTITY MasterStateMachine IS PORT
 (
     clk 	: IN STD_LOGIC; -- clock.
     clr 	: IN STD_LOGIC; -- async. clear.
-    state   : OUT STD_LOGIC_LOGIC(7 downto 0) -- output.
+    state   : OUT STD_LOGIC_LOGIC(5 downto 0) -- output.
 );
 END MasterStateMachine;
 
 ARCHITECTURE description OF MasterStateMachine IS
 
 signal	Pre_Q	: STD_LOGIC_VECTOR (2 downto 0);
-signal	state	: STD_LOGIC_VECTOR (7 downto 0);
+signal	state	: STD_LOGIC_VECTOR (5 downto 0);
 
 BEGIN
-	if 		Pre_Q = '000' then PreQ <= "00000001"
-	elsif	Pre_Q = '001' then PreQ <= "00000010"
-	elsif	Pre_Q = '011' then PreQ <= "00000100"
-	elsif	Pre_Q = '010' then PreQ <= "00001000"
-	elsif	Pre_Q = '100' then PreQ <= "00010000"
-	elsif	Pre_Q = '110' then PreQ <= "00100000"
-	elsif	Pre_Q = '111' then PreQ <= "01000000"
-	elsif	Pre_Q = '100' then PreQ <= "10000000";
+	if 		Pre_Q = '000' then state <= "000001"
+	elsif	Pre_Q = '001' then state <= "000010"
+	elsif	Pre_Q = '011' then state <= "000100"
+	elsif	Pre_Q = '010' then state <= "001000"
+	elsif	Pre_Q = '110' then state <= "010000"
+	elsif	Pre_Q = '100' then state <= "100000";
     process(clk, clr, Pre_Q)
     begin
         if clr = '1' then
