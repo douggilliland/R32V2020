784e9a2e957bd0d4ebd4667270045f84a95285ee
Doug Gilliland <doug@douglasgilliland.com>
2019-05-13 10:51:34 -0400

    Updates to R32V2020 top level

--- VHDL/Top_Level_Builds/R32V2020_Build_V001/R32V2020.vhd
+++ VHDL/Top_Level_Builds/R32V2020_Build_V001/R32V2020.vhd
@@ -10,72 +10,72 @@ entity R32V2020 is
 		n_reset		: in std_logic;
 		CLOCK_50		: in std_logic;
 		
-		rxd			: in std_logic;
-		txd			: out std_logic;
-		rts			: out std_logic;
+		-- switch0		: in std_logic;
+		-- switch1		: in std_logic;
+		-- switch2		: in std_logic;
 
-		VoutVect		: out std_logic_vector(17 downto 0); -- rrrrr,gggggg,bbbbb,hsync,vsync
-		
-		switch0		: in std_logic;
-		switch1		: in std_logic;
-		switch2		: in std_logic;
+		-- LED1			: out std_logic;
+		-- LED2			: out std_logic;
+		-- LED3			: out std_logic;
+		-- LED4			: out std_logic;
 
-		LED1			: out std_logic;
-		LED2			: out std_logic;
-		LED3			: out std_logic;
-		LED4			: out std_logic;
+		-- BUZZER		: out std_logic;
 
-		BUZZER		: out std_logic;
+		SerRxd			: in std_logic;
+		SerTxd			: out std_logic;
+		SerRts			: out std_logic;
 
-		ps2Clk		: in std_logic;
-		ps2Data		: in std_logic;
+		VideoVect		: out std_logic_vector(17 downto 0); -- rrrrr,gggggg,bbbbb,hsync,vsync
 		
 		Anode_Activate : out std_logic_vector(3 downto 0);
-		LED_out			: out std_logic_vector(6 downto 0)
+		LED_out			: out std_logic_vector(6 downto 0);
 
+		ps2Clk		: in std_logic;
+		ps2Data		: in std_logic		
 		);
 end R32V2020;
 
 architecture struct of R32V2020 is
 
-signal	Op_HCF : std_logic;
-signal	Op_NOP : std_logic;
-signal	Op_RES : std_logic;
-signal	Op_ADS : std_logic;
-signal	Op_CMP : std_logic;
-signal	Op_MUL : std_logic;
-signal	Op_MAO : std_logic;
-signal	Op_ORS  : std_logic;
-signal	Op_ARS  : std_logic;
-signal	Op_XRS  : std_logic;
-signal	Op_LS1  : std_logic;
-signal	Op_RS1  : std_logic;
-signal	Op_LR1  : std_logic;
-signal	Op_RR1  : std_logic;
-signal	Op_RA1  : std_logic;
-signal	Op_ENS  : std_logic;
-signal	Op_LIL  : std_logic;
-signal	Op_LIU  : std_logic;
-signal	Op_LDB  : std_logic;
-signal	Op_SDB  : std_logic;
-signal	Op_LDS  : std_logic;
-signal	Op_SDS  : std_logic;
-signal	Op_SDL  : std_logic;
-signal	Op_LPB  : std_logic;
-signal	Op_SPB  : std_logic;
-signal	Op_LPS  : std_logic;
-signal	Op_SPS  : std_logic;
-signal	Op_LPL  : std_logic;
-signal	Op_SPL  : std_logic;
-signal	Op_PSS  : std_logic;
-signal	Op_PUS  : std_logic;
-signal	Op_JSR  : std_logic;
-signal	Op_RTS  : std_logic;
-signal	Op_BRA  : std_logic;
-signal	Op_BCS  : std_logic;
-signal	Op_BCC  : std_logic;
-signal	Op_BOV  : std_logic;
-signal	Op_BEQ  : std_logic;
+signal	Op_HCF : std_logic;		-- Halt and Catch Fire
+signal	Op_NOP : std_logic;		-- No operation
+signal	Op_RES : std_logic;		-- Reset CPU
+signal	Op_ADS : std_logic;		-- Add 2 regs and store in 3rd
+signal	Op_CMP : std_logic;		-- Compare 2 regs and set cond codes
+signal	Op_MUL : std_logic;		-- Multiply 2 regs and store in 3rd
+--signal	Op_MAO : std_logic;
+signal	Op_ORS  : std_logic;	-- OR 2 regs and store in 3rd
+signal	Op_ARS  : std_logic;	-- AND 2 regs and store in 3rd
+signal	Op_XRS  : std_logic;	-- XOR 2 regs and store in 3rd
+signal	Op_LS1  : std_logic;	-- Logical Shift left by 1
+signal	Op_RS1  : std_logic;	-- Logical Shift right by 1
+signal	Op_LR1  : std_logic;	-- Rotate left by 1
+signal	Op_RR1  : std_logic;	-- Rotate right by 12
+signal	Op_RA1  : std_logic;	-- Right arithmetic shift
+signal	Op_ENS  : std_logic;	-- Swap endian
+signal	Op_LIL  : std_logic;	-- Load Register Immediate Lower
+signal	Op_LIU  : std_logic;	-- Load Register Immediate Upper
+signal	Op_LDB  : std_logic;	-- Load Data Byte
+signal	Op_SDB  : std_logic;	-- Store Data Byte
+signal	Op_LDS  : std_logic;	-- Load Data Short
+signal	Op_SDS  : std_logic;	-- Store Data Short
+signal	Op_LDL  : std_logic;	-- Load Data Long
+signal	Op_SDL  : std_logic;	-- Store Data Long
+signal	Op_LPB  : std_logic;	-- Load Peripheral Byte
+signal	Op_SPB  : std_logic;	-- Store Peripheral Byte
+signal	Op_LPS  : std_logic;	-- Load Peripheral Short
+signal	Op_SPS  : std_logic;	-- Store Peripheral Short
+signal	Op_LPL  : std_logic;	-- Load Peripheral Long
+signal	Op_SPL  : std_logic;	-- Store Peripheral Long
+signal	Op_PSS  : std_logic;	-- Push register to Stack
+signal	Op_PUS  : std_logic;	-- Pull register from Stack
+signal	Op_JSR  : std_logic;	-- Jump to Subroutine
+signal	Op_RTS  : std_logic;	-- Return from Subroutine
+signal	Op_BRA  : std_logic;	-- Branch Always
+signal	Op_BCS  : std_logic;	-- Branch if carry is set
+signal	Op_BCC  : std_logic;	-- Branch if carry is clear
+signal	Op_BOV  : std_logic;	-- Branch if overflow
+signal	Op_BEQ  : std_logic;	-- Branch if equal
 
 signal	Video_Clk	: std_logic;
 
@@ -88,16 +88,16 @@ signal	CCR			: std_logic_vector(31 downto 0);
 signal	writeRegFileStrobe	: std_logic;
 signal	dataIntoRegisterFile	: std_logic_vector(31 downto 0);
 
-signal	InstructionAddress	: std_logic_vector(31 downto 0);
-signal	BlockInstructionRamData					: std_logic_vector(31 downto 0);
+signal	InstructionRomAddress	: std_logic_vector(31 downto 0);
+signal	InstructionRomData					: std_logic_vector(31 downto 0);
 
-signal	StackAddress	: std_logic_vector(31 downto 0);
+signal	StackRamAddress	: std_logic_vector(31 downto 0);
 signal	dataToStackRam			: std_logic_vector(31 downto 0);
-signal	writeStackRam			: std_logic;
+signal	writeStackRamEn			: std_logic;
 signal	dataFromStackRam		: std_logic_vector(31 downto 0);
 
+signal	DataRamAddress		: std_logic_vector(31 downto 0);
 signal	dataToDataRam				: std_logic_vector(31 downto 0);
-signal	DataAddress		: std_logic_vector(31 downto 0);
 signal	dataRamWriteAddress		: std_logic_vector(31 downto 0);
 signal	writeToDataRamEnable		: std_logic;
 signal	dataFromDataRam			: std_logic_vector(31 downto 0);
@@ -119,7 +119,7 @@ begin
 	
 	opcodeDecoder : entity work.OpCodeDecoder
 	port map (
-		InstrOpCode => BlockInstructionRamData(31 downto 24),
+		InstrOpCode => InstructionRomData(31 downto 24),
 		-- Category = System
 		Op_HCF => Op_HCF,
 		Op_NOP => Op_NOP,
@@ -127,7 +127,6 @@ begin
 		-- Category = ALU
 		Op_ADS => Op_ADS,
 		Op_MUL => Op_MUL,
-		Op_MAO => Op_MAO,
 		Op_ORS => Op_ORS,
 		Op_ARS => Op_ARS,
 		Op_XRS => Op_XRS,
@@ -187,18 +186,28 @@ begin
 
 	Instr_ROM : ENTITY work.BlockRom_Instruction
 	PORT MAP (
-		address => InstructionAddress(7 downto 0),
+		address => InstructionRomAddress(7 downto 0),
 		clock => CLOCK_50,
-		q => BlockInstructionRamData
+		q => InstructionRomData
 	);
 
-	StackRAM : ENTITY work.BlockRam_Stack
+	Stack_RAM : ENTITY work.BlockRam_Stack
 	PORT MAP	(
-		address => StackAddress(7 downto 0),
+		address => StackRamAddress(7 downto 0),
 		clock => CLOCK_50,
 		data => dataToStackRam,
-		wren => writeStackRam,
+		wren => writeStackRamEn,
 		q => dataFromStackRam
+	);
+
+	Data_RAM : ENTITY work.BlockRam_Data
+	PORT MAP (
+		clock => CLOCK_50,
+		data => dataToDataRam,
+		rdaddress => DataRamAddress(7 downto 0),
+		wraddress => DataRamAddress(7 downto 0),
+		wren => writeToDataRamEnable,
+		q => dataFromDataRam
 	);
 
 	RegisterFile : entity work.RegisterFile
@@ -206,28 +215,18 @@ begin
 		clk			=> CLOCK_50,
 		clear			=> not n_reset,
 		wrStrobe		=> writeRegFileStrobe,
-		wrRegSel		=> BlockInstructionRamData(23 downto 20),
-		rdRegSelA	=> BlockInstructionRamData(15 downto 12),
-		rdRegSelB	=> BlockInstructionRamData(19 downto 16),
+		wrRegSel		=> InstructionRomData(23 downto 20),
+		rdRegSelA	=> InstructionRomData(15 downto 12),
+		rdRegSelB	=> InstructionRomData(19 downto 16),
 		regDataIn	=> dataIntoRegisterFile,
 		regDataOutA	=> regDataA,
 		regDataOutB	=> regDataB,
 		i_CCR						=> CondCodeBits,
-		o_StackAddress			=> StackAddress,
+		o_StackRamAddress			=> StackRamAddress,
 		o_PeripheralAddress	=> PeripheralAddress,
-		o_DataAddress			=> DataAddress,
-		o_InstructionAddress	=> InstructionAddress,
+		o_DataRamAddress			=> DataRamAddress,
+		o_InstructionRomAddress	=> InstructionRomAddress,
 		o_CCR						=> CCR
-	);
-
-	DataRAM : ENTITY work.BlockRam_Data
-	PORT MAP (
-		clock => CLOCK_50,
-		data => dataToDataRam,
-		rdaddress => DataAddress(7 downto 0),
-		wraddress => DataAddress(7 downto 0),
-		wren => writeToDataRamEnable,
-		q => dataFromDataRam
 	);
 
 	Peripherals : entity work.PeripheralInterface
@@ -240,12 +239,12 @@ begin
 		peripheralDataOut		=> peripheralDataOut,
 		peripheralRdStrobe	=> peripheralRdStrobe,
 		peripheralWrStrobe	=> peripheralWrStrobe,
-		rxd						=> rxd,
-		txd						=> txd,
-		VoutVect					=> VoutVect,
+		rxd						=> SerRxd,
+		txd						=> SerTxd,
+		rts						=> SerRts,
+		VoutVect					=> VideoVect,
 		PS2_CLK					=> ps2Clk,
 		PS2_DATA					=> ps2Data
-
 		);
 
 end;
