bde2eb4723f3cfaa86b2feb813bca60334eaa71f
Doug Gilliland <doug@douglasgilliland.com>
2019-05-25 07:07:42 -0400

    Fix sensitivity list to add ld

--- VHDL/Components/Registers/REG_16.vhd
+++ VHDL/Components/Registers/REG_16.vhd
@@ -4,10 +4,10 @@ use  IEEE.STD_LOGIC_ARITH.all;
 use  IEEE.STD_LOGIC_UNSIGNED.all;
 
 ENTITY REG_16 IS PORT(
+    clk : IN STD_LOGIC; -- clock.
     d   : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
     ld  : IN STD_LOGIC; -- load/enable.
     clr : IN STD_LOGIC; -- async. clear.
-    clk : IN STD_LOGIC; -- clock.
     q   : OUT STD_LOGIC_VECTOR(15 DOWNTO 0) -- output
 );
 END REG_16;
@@ -15,7 +15,7 @@ END REG_16;
 ARCHITECTURE description OF REG_16 IS
 
 BEGIN
-    process(clk, clr)
+    process(clk, clr, ld)
     begin
         if clr = '1' then
             q <= x"0000";
