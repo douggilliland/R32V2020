155b748639f7086b6aec1526baac5bb6a0bce513
Doug Gilliland <doug@douglasgilliland.com>
2019-05-26 13:06:40 -0400

    Update C008-BRA_Test.asm

--- VHDL/Programs/C008-BRA_Test/C008-BRA_Test.asm
+++ VHDL/Programs/C008-BRA_Test/C008-BRA_Test.asm
@@ -5,4 +5,4 @@ next:
 	nop
 	liu r8,0x0000	; Should set the upper 16-bits of r8 to the upper 16 bits of start
 	lil r8,0x0002	; Should set the lower 16-bits of r8 to the lower 16 bits of start
-	bra next		; Copy r8 into r7 - causing a jump to start
+	bra next		; Relative branch (-3 in this case)
