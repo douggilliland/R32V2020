7d84b61111d6f3a0c478efa6c669235aceb0ad88
Doug Gilliland <doug@douglasgilliland.com>
2019-05-26 16:31:56 -0400

    Opcode changes

--- VHDL/Components/OpCodeDecoder/OpCodeDecoder.vhd
+++ VHDL/Components/OpCodeDecoder/OpCodeDecoder.vhd
@@ -16,6 +16,7 @@ entity OpCodeDecoder is
 		-- Category = ALU
 		Op_ADS		: buffer std_logic;	-- Add and store in reg
 		Op_MUL		: buffer std_logic;	-- Multiply and store in reg
+		Op_CMP		: buffer std_logic;	-- Compare two registers and set CCR bits accordingly
 		Op_ORS		: buffer std_logic;	-- Logical OR registers and store in reg
 		Op_ARS		: buffer std_logic;	-- Logical AND registers and store in reg
 		Op_XRS		: buffer std_logic;	-- Logical XOR registers and store in reg
@@ -25,7 +26,6 @@ entity OpCodeDecoder is
 		Op_RR1		: buffer std_logic;	-- Logical Rotate register Right by 1 and store in reg
 		Op_RA1		: buffer std_logic;	-- Arithmetic shift register Right by 1 and store in reg
 		Op_ENS		: buffer std_logic;	-- Swap Endian of register and store in reg
-		Op_CMP		: buffer std_logic;	-- Compare two registers and set CCR bits accordingly
 		-- Category = Immediate values
 		Op_LIL		: buffer std_logic;	-- Load Immediate lower short
 		Op_LIU		: buffer std_logic;	-- Load Immediate upper short
@@ -49,14 +49,13 @@ entity OpCodeDecoder is
 		Op_SSS		: out std_logic;	-- Store to Stack memory
 		Op_LSS		: buffer std_logic;	-- Load from Stack memory
 		-- Category = Flow Control
-		Op_JSR 		: out std_logic;	-- Jump Subroutine
-		Op_RTS 		: out std_logic;	-- Return Subroutine
 		Op_BRA 		: out std_logic;	-- Branch Always
 		Op_BCS 		: out std_logic;	-- Branch if Carry Set
 		Op_BCC 		: out std_logic;	-- Branch if Carry Clear
 		Op_BEZ 		: out std_logic;	-- Branch if ALU result is Equal to Zero
 		Op_BE1 		: out std_logic;	-- Branch if ALU result is Equal to One
-		Op_BOV 		: out std_logic;	-- Branch if Overflow
+		Op_BGT 		: out std_logic;	-- Branch if Greater Than
+		Op_BLT 		: out std_logic;	-- Branch if Less Than
 		Op_BEQ 		: out std_logic;	-- Branch if Equal
 		o_WrRegFile	: out std_logic	-- Register File gets output of opcode
 	);
@@ -71,55 +70,48 @@ signal	LdSt_OpCode		: std_logic;
 signal	Perip_OpCode	: std_logic;
 signal	Stack_OpCode	: std_logic;
 signal	FlowCtl_OpCode	: std_logic;
--- System
-constant NOP	: std_Logic_Vector(7 downto 0) := "00000000";
-constant HCF	: std_Logic_Vector(7 downto 0) := "00000001";
-constant RES	: std_Logic_Vector(7 downto 0) := "00000010";
--- ALU
-constant ADS	: std_Logic_Vector(7 downto 0) := "00100000";
-constant MUL	: std_Logic_Vector(7 downto 0) := "00100001";
-constant CMP 	: std_Logic_Vector(7 downto 0) := "00100010";
-constant ORS 	: std_Logic_Vector(7 downto 0) := "00101000";
-constant ARS 	: std_Logic_Vector(7 downto 0) := "00101001";
-constant XRS 	: std_Logic_Vector(7 downto 0) := "00101010";
-constant LS1 	: std_Logic_Vector(7 downto 0) := "00110000";
-constant RS1 	: std_Logic_Vector(7 downto 0) := "00110001";
-constant LR1 	: std_Logic_Vector(7 downto 0) := "00110010";
-constant RR1 	: std_Logic_Vector(7 downto 0) := "00110011";
-constant RA1 	: std_Logic_Vector(7 downto 0) := "00110100";
-constant ENS 	: std_Logic_Vector(7 downto 0) := "00111000";
--- Immediate Opcodes
-constant LIL 	: std_Logic_Vector(7 downto 0) := "01000000";
-constant LIU 	: std_Logic_Vector(7 downto 0) := "01000001";
--- Load/Store Data Memory Opcodes
-constant LDB 	: std_Logic_Vector(7 downto 0) := "01100000";
-constant SDB 	: std_Logic_Vector(7 downto 0) := "01100001";
-constant LDS 	: std_Logic_Vector(7 downto 0) := "01100010";
-constant SDS 	: std_Logic_Vector(7 downto 0) := "01100011";
-constant LDL 	: std_Logic_Vector(7 downto 0) := "01100100";
-constant SDL 	: std_Logic_Vector(7 downto 0) := "01100101";
--- Peripheral I/O Opcodes
-constant LPB 	: std_Logic_Vector(7 downto 0) := "10000000";
-constant SPB 	: std_Logic_Vector(7 downto 0) := "10000001";
-constant LPS 	: std_Logic_Vector(7 downto 0) := "10000010";
-constant SPS 	: std_Logic_Vector(7 downto 0) := "10000011";
-constant LPL 	: std_Logic_Vector(7 downto 0) := "10000100";
-constant SPL 	: std_Logic_Vector(7 downto 0) := "10000101";
--- Stack Opcodes
-constant PSS 	: std_Logic_Vector(7 downto 0) := "10100000";
-constant PUS 	: std_Logic_Vector(7 downto 0) := "10100001";
-constant SSS 	: std_Logic_Vector(7 downto 0) := "10100010";
-constant LSS 	: std_Logic_Vector(7 downto 0) := "10100011";
--- Flow Control
-constant JSR 	: std_Logic_Vector(7 downto 0) := "11000000";
-constant RTS 	: std_Logic_Vector(7 downto 0) := "11000001";
-constant BRA 	: std_Logic_Vector(7 downto 0) := "11010000";
-constant BCS 	: std_Logic_Vector(7 downto 0) := "11010001";
-constant BCC 	: std_Logic_Vector(7 downto 0) := "11010010";
-constant BEZ 	: std_Logic_Vector(7 downto 0) := "11010011";
-constant BE1  	: std_Logic_Vector(7 downto 0) := "11010100";
-constant BOV 	: std_Logic_Vector(7 downto 0) := "11010101";
-constant BEQ  	: std_Logic_Vector(7 downto 0) := "11010110";
+
+constant NOP : std_Logic_Vector(7 downto 0) := "00000000";
+constant HCF : std_Logic_Vector(7 downto 0) := "00000001";
+constant RES : std_Logic_Vector(7 downto 0) := "00000010";
+constant ADS : std_Logic_Vector(7 downto 0) := "00100000";
+constant MUL : std_Logic_Vector(7 downto 0) := "00100001";
+constant CMP : std_Logic_Vector(7 downto 0) := "00100010";
+constant ORS : std_Logic_Vector(7 downto 0) := "00101000";
+constant ARS : std_Logic_Vector(7 downto 0) := "00101001";
+constant XRS : std_Logic_Vector(7 downto 0) := "00101010";
+constant LS1 : std_Logic_Vector(7 downto 0) := "00110000";
+constant RS1 : std_Logic_Vector(7 downto 0) := "00110001";
+constant LR1 : std_Logic_Vector(7 downto 0) := "00110010";
+constant RR1 : std_Logic_Vector(7 downto 0) := "00110011";
+constant RA1 : std_Logic_Vector(7 downto 0) := "00110100";
+constant ENS : std_Logic_Vector(7 downto 0) := "00111000";
+constant LIL : std_Logic_Vector(7 downto 0) := "01000000";
+constant LIU : std_Logic_Vector(7 downto 0) := "01000001";
+constant LDB : std_Logic_Vector(7 downto 0) := "01100000";
+constant SDB : std_Logic_Vector(7 downto 0) := "01100001";
+constant LDS : std_Logic_Vector(7 downto 0) := "01100010";
+constant SDS : std_Logic_Vector(7 downto 0) := "01100011";
+constant LDL : std_Logic_Vector(7 downto 0) := "01100100";
+constant SDL : std_Logic_Vector(7 downto 0) := "01100101";
+constant LPB : std_Logic_Vector(7 downto 0) := "10000000";
+constant SPB : std_Logic_Vector(7 downto 0) := "10000001";
+constant LPS : std_Logic_Vector(7 downto 0) := "10000010";
+constant SPS : std_Logic_Vector(7 downto 0) := "10000011";
+constant LPL : std_Logic_Vector(7 downto 0) := "10000100";
+constant SPL : std_Logic_Vector(7 downto 0) := "10000101";
+constant PSS : std_Logic_Vector(7 downto 0) := "10100000";
+constant PUS : std_Logic_Vector(7 downto 0) := "10100001";
+constant SSS : std_Logic_Vector(7 downto 0) := "10100010";
+constant LSS : std_Logic_Vector(7 downto 0) := "10100011";
+constant BRA : std_Logic_Vector(7 downto 0) := "11000000";
+constant BCS : std_Logic_Vector(7 downto 0) := "11000001";
+constant BCC : std_Logic_Vector(7 downto 0) := "11000010";
+constant BEZ : std_Logic_Vector(7 downto 0) := "11000011";
+constant BE1 : std_Logic_Vector(7 downto 0) := "11000100";
+constant BGT : std_Logic_Vector(7 downto 0) := "11000101";
+constant BLT : std_Logic_Vector(7 downto 0) := "11000110";
+constant BEQ : std_Logic_Vector(7 downto 0) := "11000111";
 
 begin
 
