363b9bb6344bd690560b8fad4a753ffde9fdfbf0
Doug Gilliland <doug@douglasgilliland.com>
2019-05-26 06:15:29 -0400

    Example 6 update

--- VHDL/Programs/C006-Address/C006-Address.asm
+++ VHDL/Programs/C006-Address/C006-Address.asm
@@ -1,4 +1,4 @@
 start:
-	liu r8,start	; Should set the upper 16-bits of r8 to the upper 16 bits of start
-	lil r8,start	; Should set the lower 16-bits of r8 to the lower 16 bits of start
-	jmp r8			; Copy r8 into r7 - causing a jump to start
+	liu r8,0x0000	; Should set the upper 16-bits of r8 to the upper 16 bits of start
+	lil r8,0x0000	; Should set the lower 16-bits of r8 to the lower 16 bits of start
+	bra start		; Copy r8 into r7 - causing a jump to start
