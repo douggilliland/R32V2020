8c1a3303a4a3f3a75f5568cebc7dc0765861720d
Doug Gilliland <doug@douglasgilliland.com>
2019-05-27 06:36:41 -0400

    Test CCR flags

--- VHDL/Programs/C010-CCR_Test/C010-CCR_Test.asm
+++ VHDL/Programs/C010-CCR_Test/C010-CCR_Test.asm
@@ -1,10 +1,26 @@
 ; Code that jumps all over the place
 start:
 	nop
+	nop
 	ads r8,r0,r0
+	nop		; make sure the CCR "sticks" for non-ALU operations
+	nop
 	ads r8,r0,r1
+	nop		; make sure the CCR "sticks" for non-ALU operations
+	nop
 	ads r8,r0,r2
-	cmp r3,r0,r0
-	cmp r3,r0,r1
-	cmp r3,r1,r0
+	nop		; make sure the CCR "sticks" for non-ALU operations
+	nop
+	ads r8,r3,r1
+	nop		; make sure the CCR "sticks" for non-ALU operations
+	nop
+	cmp r0,r0
+	nop		; make sure the CCR "sticks" for non-ALU operations
+	nop
+	cmp r0,r1
+	nop		; make sure the CCR "sticks" for non-ALU operations
+	nop
+	cmp r1,r0
+	nop		; make sure the CCR "sticks" for non-ALU operations
+	nop
 	bra start
