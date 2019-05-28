8736695f49d389d7a2789e16842814f1da0ab94c
Doug Gilliland <doug@douglasgilliland.com>
2019-05-28 05:41:00 -0400

    Screen clear and put character subroutines

---
+++ VHDL/Programs/C015-Screen_Clear/C015-Screen_Clear.asm
@@ -0,0 +1,70 @@
+; Peripheral Test
+; Put characters on the screen
+start:
+	nop				; need a nop until I fix the first fetch
+	bra	clearScreen
+doneClear:
+	ads r5,r0,r0	; start of screen character memory
+	ads r8,r0,r0	; clear the character
+	lil	r8,0x0048	; ASCII 'H'
+	ads	r9,r7,r1
+	bra	putChar
+	lil	r8,0x0065	; ASCII 'e'
+	ads	r9,r7,r1
+	bra	putChar
+	lil	r8,0x006C	; ASCII 'l'
+	ads	r9,r7,r1
+	bra	putChar
+	lil	r8,0x006C	; ASCII 'l'
+	ads	r9,r7,r1
+	bra	putChar
+	lil	r8,0x006F	; ASCII 'o'
+	ads	r9,r7,r1
+	bra	putChar
+	lil	r8,0x002C	; ASCII ','
+	ads	r9,r7,r1
+	bra	putChar
+	lil	r8,0x0020	; ASCII ' '
+	ads	r9,r7,r1
+	bra	putChar
+	lil	r8,0x0057	; ASCII 'W'
+	ads	r9,r7,r1
+	bra	putChar
+	lil	r8,0x006F	; ASCII 'o'
+	ads	r9,r7,r1
+	bra	putChar
+	lil	r8,0x0072	; ASCII 'r'
+	ads	r9,r7,r1
+	bra	putChar
+	lil	r8,0x006C	; ASCII 'l'
+	ads	r9,r7,r1
+	bra	putChar
+	lil	r8,0x0064	; ASCII 'd'
+	ads	r9,r7,r1
+	bra	putChar
+	lil	r8,0x0021	; ASCII '!'
+	ads	r9,r7,r1
+	bra	putChar
+	lil	r8,0x0020	; ASCII ' '
+	ads	r9,r7,r1
+	bra	putChar
+	hcf
+; Clear the screen routine
+clearScreen:
+	ads r5,r0,r0	; start of screen character memory
+	ads r8,r0,r0	; clear the character
+	lil	r8,0x0020	; fill with spaces
+	ads r9,r0,r0	; screen count
+	lil r9,0x7FE	; loopCount	
+looper:
+	spb r8			; put the character to the screen
+	ads	r5,r5,r1	; Increment screen pointer
+	ads r9,r9,r2	; decrement character counter
+	bne	looper		; loop until complete
+	bra	doneClear
+; put a character to the screen and increment the address
+putChar:
+	spb	r8			; write character to peripheral bus
+	ads	r5,r5,r1	; Go to the next character position	
+	ads	r7,r9,r1	; return to calling routine (r9 is the calling address)
+	
--- VHDL/Top_Level_Builds/R32V2020_Build_V001/db/ip/sld4e341988/submodules/alt_sld_fab_alt_sld_fab_ident.sv
+++ VHDL/Top_Level_Builds/R32V2020_Build_V001/db/ip/sld4e341988/submodules/alt_sld_fab_alt_sld_fab_ident.sv
@@ -20,7 +20,7 @@
 // Altera Identification hub
 //
 // Parameters
-//   DESIGN_HASH   : e03de8cad73e0a6761c6
+//   DESIGN_HASH   : 22124b9c6249cba44528
 //   COUNT         : 1
 //   ROM_WIDTHS    : 4
 //   LATENCIES     : 0
@@ -39,7 +39,7 @@ module alt_sld_fab_alt_sld_fab_ident
 );
 
 
-wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'he03de8cad73e0a6761c6 };
+wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h22124b9c6249cba44528 };
 
 reg [3:0] result_0;
 always @(address_0 or data_0) begin
