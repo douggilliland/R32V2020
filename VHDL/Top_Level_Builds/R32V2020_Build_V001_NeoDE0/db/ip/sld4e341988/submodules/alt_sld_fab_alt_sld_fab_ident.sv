59f1344cb030b4114e8c004fe7aafe66baf073dd
Doug Gilliland <doug@douglasgilliland.com>
2019-05-29 16:15:46 -0400

    Compile new top for DE0-Nano board

--- VHDL/Top_Level_Builds/R32V2020_Build_V001_NeoDE0/db/ip/sld4e341988/submodules/alt_sld_fab_alt_sld_fab_ident.sv
+++ VHDL/Top_Level_Builds/R32V2020_Build_V001_NeoDE0/db/ip/sld4e341988/submodules/alt_sld_fab_alt_sld_fab_ident.sv
@@ -20,7 +20,7 @@
 // Altera Identification hub
 //
 // Parameters
-//   DESIGN_HASH   : 8b0e56185e2818665487
+//   DESIGN_HASH   : 60b2c94fad68535b26af
 //   COUNT         : 1
 //   ROM_WIDTHS    : 4
 //   LATENCIES     : 0
@@ -39,7 +39,7 @@ module alt_sld_fab_alt_sld_fab_ident
 );
 
 
-wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h8b0e56185e2818665487 };
+wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h60b2c94fad68535b26af };
 
 reg [3:0] result_0;
 always @(address_0 or data_0) begin
