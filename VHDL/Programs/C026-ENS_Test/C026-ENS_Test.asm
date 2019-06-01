5109ec031c7dfc21ef6bbdc59d9c5b7961fd1a67
Doug Gilliland <doug@douglasgilliland.com>
2019-06-01 16:30:40 -0400

    Tested endian swap on screen write

---
+++ VHDL/Programs/C026-ENS_Test/C026-ENS_Test.asm
@@ -0,0 +1,39 @@
+start:
+	ads	par,r0,r0	; start of screen
+	liu	r8,0x4865	; ASCII 'He'
+	lil	r8,0x6C6C	; ASCII 'll'
+	ens r9,r8
+	spl	r9
+	ads	par,par,r1	; next screen character
+	rs1	r9,r9
+	rs1	r9,r9
+	rs1	r9,r9
+	rs1	r9,r9
+	rs1	r9,r9
+	rs1	r9,r9
+	rs1	r9,r9
+	rs1	r9,r9
+	spl	r9
+	ads	par,par,r1	; next screen character
+	rs1	r9,r9
+	rs1	r9,r9
+	rs1	r9,r9
+	rs1	r9,r9
+	rs1	r9,r9
+	rs1	r9,r9
+	rs1	r9,r9
+	rs1	r9,r9
+	spl	r9
+	ads	par,par,r1	; next screen character
+	rs1	r9,r9
+	rs1	r9,r9
+	rs1	r9,r9
+	rs1	r9,r9
+	rs1	r9,r9
+	rs1	r9,r9
+	rs1	r9,r9
+	rs1	r9,r9
+	spl	r9
+loop:
+	bra	loop
+	
--- VHDL/Top_Level_Builds/R32V2020_Build_V001/db/ip/sld4e341988/submodules/alt_sld_fab_alt_sld_fab_ident.sv
+++ VHDL/Top_Level_Builds/R32V2020_Build_V001/db/ip/sld4e341988/submodules/alt_sld_fab_alt_sld_fab_ident.sv
@@ -20,7 +20,7 @@
 // Altera Identification hub
 //
 // Parameters
-//   DESIGN_HASH   : f1d8a032b37323cea1c5
+//   DESIGN_HASH   : 8e3e724af6fcc6788b08
 //   COUNT         : 1
 //   ROM_WIDTHS    : 4
 //   LATENCIES     : 0
@@ -39,7 +39,7 @@ module alt_sld_fab_alt_sld_fab_ident
 );
 
 
-wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'hf1d8a032b37323cea1c5 };
+wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h8e3e724af6fcc6788b08 };
 
 reg [3:0] result_0;
 always @(address_0 or data_0) begin
