4921b5077f641e11ab1db7114fe44cb97ef465f4
Doug Gilliland <doug@douglasgilliland.com>
2019-06-01 14:21:30 -0400

    C025-Store return address on stack and load from stack when returning

---
+++ VHDL/Programs/C025-Hello_Stack/C025-Hello_Stack.asm
@@ -0,0 +1,75 @@
+; Peripheral Test
+; Put characters on the screen
+start:
+	ads	SAR,r0,r0	; Use Stack for return address
+	sss	r7			; push the call address -1
+	bra	clearScreen
+doneClear:
+	ads r5,r0,r0	; start of screen character memory
+	ads r8,r0,r0	; clear the character
+	lil	r8,0x0048	; ASCII 'H'
+	sss	r7
+	bra	putChar
+	lil	r8,0x0065	; ASCII 'e'
+	sss	r7
+	bra	putChar
+	lil	r8,0x006C	; ASCII 'l'
+	sss	r7
+	bra	putChar
+	lil	r8,0x006C	; ASCII 'l'
+	sss	r7
+	bra	putChar
+	lil	r8,0x006F	; ASCII 'o'
+	sss	r7
+	bra	putChar
+	lil	r8,0x002C	; ASCII ','
+	sss	r7
+	bra	putChar
+	lil	r8,0x0020	; ASCII ' '
+	sss	r7
+	bra	putChar
+	lil	r8,0x0057	; ASCII 'W'
+	sss	r7
+	bra	putChar
+	lil	r8,0x006F	; ASCII 'o'
+	sss	r7
+	bra	putChar
+	lil	r8,0x0072	; ASCII 'r'
+	sss	r7
+	bra	putChar
+	lil	r8,0x006C	; ASCII 'l'
+	sss	r7
+	bra	putChar
+	lil	r8,0x0064	; ASCII 'd'
+	sss	r7
+	bra	putChar
+	lil	r8,0x0021	; ASCII '!'
+	sss	r7
+	bra	putChar
+	lil	r8,0x0020	; ASCII ' '
+	sss	r7
+	bra	putChar
+	hcf
+; Clear the screen routine
+clearScreen:
+	ads r5,r0,r0	; start of screen character memory
+	ads r8,r0,r0	; clear the character
+	lil	r8,0x0020	; fill with spaces
+	ads r9,r0,r0	; screen count
+	lil r9,0x7FE	; loopCount	
+looper:
+	spb r8			; put the character to the screen
+	ads	r5,r5,r1	; Increment screen pointer
+	ads r9,r9,r2	; decrement character counter
+	bne	looper		; loop until complete
+	lss	r10			; load calling address
+	ads	r10,r10,r1	; skip the call
+	ads	r7,r10,r1	; jump to the next address (rts)
+; put a character to the screen and increment the address
+putChar:
+	spb	r8			; write character to peripheral bus
+	ads	r5,r5,r1	; Go to the next character position	
+	lss	r10			; load calling address
+	ads	r10,r10,r1	; skip the call
+	ads	r7,r10,r1	; jump to the next address (rts)
+	
--- VHDL/Top_Level_Builds/R32V2020_Build_V001/db/ip/sld4e341988/submodules/alt_sld_fab_alt_sld_fab_ident.sv
+++ VHDL/Top_Level_Builds/R32V2020_Build_V001/db/ip/sld4e341988/submodules/alt_sld_fab_alt_sld_fab_ident.sv
@@ -20,7 +20,7 @@
 // Altera Identification hub
 //
 // Parameters
-//   DESIGN_HASH   : 462dc3ea9eb146cd66fd
+//   DESIGN_HASH   : f1d8a032b37323cea1c5
 //   COUNT         : 1
 //   ROM_WIDTHS    : 4
 //   LATENCIES     : 0
@@ -39,7 +39,7 @@ module alt_sld_fab_alt_sld_fab_ident
 );
 
 
-wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h462dc3ea9eb146cd66fd };
+wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'hf1d8a032b37323cea1c5 };
 
 reg [3:0] result_0;
 always @(address_0 or data_0) begin
