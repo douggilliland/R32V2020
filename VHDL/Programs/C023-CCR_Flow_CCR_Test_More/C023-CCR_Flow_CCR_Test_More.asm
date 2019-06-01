0f8b3c28dc829776da51cba879acf7cc75cb3506
Doug Gilliland <doug@douglasgilliland.com>
2019-06-01 10:27:23 -0400

    Tested all branch opcodes positively and negatively

---
+++ VHDL/Programs/C023-CCR_Flow_CCR_Test_More/C023-CCR_Flow_CCR_Test_More.asm
@@ -0,0 +1,64 @@
+; Bxx Checks
+; Test all branches with both sets of conditions
+start:
+	cmp r0,r0		; test cmp and bne/beq
+	bne failed		; bne not taken
+	beq	skip1		; beq taken
+	bra	failed
+skip1:
+	cmp r0,r1		; test cmp and beq/bne
+	beq	failed		; beq not taken
+	bne skip2		; bne taken
+	bra	failed
+skip2:
+	ads	r8,r0,r1	; test ads and bez/be1
+	bez	failed		; bez not taken
+	be1	skip3		; be1 taken
+	bra	failed
+skip3:
+	ads	r8,r0,r0	; test
+	be1	failed		; be1 not taken
+	bez	skip4		; bez taken
+	bra	failed
+skip4:
+	ads	r8,r0,r1	; test ads and bez/be1
+	bnz	skip5		; bnz taken
+	bra failed
+skip5:
+	ads	r8,r0,r0	; test bcc/bcs
+	bnz	failed		; bnz should not have been taken
+testBCCBCS:
+	ads	r8,r0,r0	; test carry clear
+	bcs	failed		; bcs should not have been taken
+	bcc	skip6		; bcc taken
+	bra	failed		
+skip6:
+	ads r8,MINUS1,MINUS1	; should set carry
+	bcc	failed		; should not take bcc
+	bcs	skip7		; should take bcs
+	bra	failed
+skip7:
+	cmp	r0,r1		; test blt/bgt
+	blt	failed		; should not take blt
+	bgt	skip8		; should take bgt
+	bra	failed
+skip8:
+	cmp	r1,r0		; test blt/bgt
+	bgt	failed		; should not take bgt
+	blt	skip9		; should take blt
+	bra	failed
+skip9:
+passed:		; send GOOD to the 7 Segment LED
+	ads	r11,r0,r0
+	lil	r11,0x3000
+	ads	par,r11,r0
+	lil	r10,0x900D
+	spl	r10
+	hcf
+failed:		; send 0BAD to the 7 Segment LED
+	ads	r11,r0,r0
+	lil	r11,0x3000
+	ads	par,r11,r0
+	lil	r10,0x0BAD
+	spl	r10
+	hcf
--- VHDL/Top_Level_Builds/R32V2020_Build_V001/db/ip/sld4e341988/submodules/alt_sld_fab_alt_sld_fab_ident.sv
+++ VHDL/Top_Level_Builds/R32V2020_Build_V001/db/ip/sld4e341988/submodules/alt_sld_fab_alt_sld_fab_ident.sv
@@ -20,7 +20,7 @@
 // Altera Identification hub
 //
 // Parameters
-//   DESIGN_HASH   : e6e9c1d2980256129168
+//   DESIGN_HASH   : 78eb3aedbbf65fb4d2cd
 //   COUNT         : 1
 //   ROM_WIDTHS    : 4
 //   LATENCIES     : 0
@@ -39,7 +39,7 @@ module alt_sld_fab_alt_sld_fab_ident
 );
 
 
-wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'he6e9c1d2980256129168 };
+wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h78eb3aedbbf65fb4d2cd };
 
 reg [3:0] result_0;
 always @(address_0 or data_0) begin
