22ce421d3507f8571df0a50037d4d93c7546bb31
Doug Gilliland <doug@douglasgilliland.com>
2019-06-06 12:09:42 -0400

    Got Elapsed Time Counter working with 7 Seg 8 Digit LED display on A4-CE6 card

--- VHDL/Top_Level_Builds/R32V2020_Build_V001_A4_CE6/db/ip/sld4e341988/submodules/alt_sld_fab_alt_sld_fab_ident.sv
+++ VHDL/Top_Level_Builds/R32V2020_Build_V001_A4_CE6/db/ip/sld4e341988/submodules/alt_sld_fab_alt_sld_fab_ident.sv
@@ -20,7 +20,7 @@
 // Altera Identification hub
 //
 // Parameters
-//   DESIGN_HASH   : fb54ef22bced763b29a9
+//   DESIGN_HASH   : 9e08317953fb97d15f58
 //   COUNT         : 1
 //   ROM_WIDTHS    : 4
 //   LATENCIES     : 0
@@ -39,7 +39,7 @@ module alt_sld_fab_alt_sld_fab_ident
 );
 
 
-wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'hfb54ef22bced763b29a9 };
+wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h9e08317953fb97d15f58 };
 
 reg [3:0] result_0;
 always @(address_0 or data_0) begin
