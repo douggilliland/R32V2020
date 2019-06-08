fcaece8b4c7c212c9f82d9dce8f73b974fdb661a
Doug Gilliland <doug@douglasgilliland.com>
2019-06-08 06:50:10 -0400

    C044 putChar and screenClear as subroutines

--- VHDL/Top_Level_Builds/R32V2020_Build_V001_A4_CE6/db/ip/sld4e341988/submodules/alt_sld_fab_alt_sld_fab_ident.sv
+++ VHDL/Top_Level_Builds/R32V2020_Build_V001_A4_CE6/db/ip/sld4e341988/submodules/alt_sld_fab_alt_sld_fab_ident.sv
@@ -20,7 +20,7 @@
 // Altera Identification hub
 //
 // Parameters
-//   DESIGN_HASH   : 9354d021933c36ba9e0b
+//   DESIGN_HASH   : 1d409242f6a46aeace2a
 //   COUNT         : 1
 //   ROM_WIDTHS    : 4
 //   LATENCIES     : 0
@@ -39,7 +39,7 @@ module alt_sld_fab_alt_sld_fab_ident
 );
 
 
-wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h9354d021933c36ba9e0b };
+wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h1d409242f6a46aeace2a };
 
 reg [3:0] result_0;
 always @(address_0 or data_0) begin
