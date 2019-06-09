11d82cffadd7acc155a23cc956c0c1e5277e45c1
Doug Gilliland <doug@douglasgilliland.com>
2019-06-09 15:46:58 -0400

    Tested loop LEDs

--- VHDL/Components/BlockRom_Instruction/BlockRom_Instruction.vhd
+++ VHDL/Components/BlockRom_Instruction/BlockRom_Instruction.vhd
@@ -62,7 +62,7 @@ BEGIN
 		address_aclr_a => "NONE",
 		clock_enable_input_a => "NORMAL",
 		clock_enable_output_a => "BYPASS",
-		init_file => "../../Programs/C051-Ring_LED_Test/C051-Ring_LED_Test_ins.HEX",
+		init_file => "../../Programs/C052-Ring_LED_Loop_Test/C052-Ring_LED_Loop_Test_ins.HEX",
 		intended_device_family => "Cyclone IV E",
 		lpm_hint => "ENABLE_RUNTIME_MOD=YES,INSTANCE_NAME=INST",
 		lpm_type => "altsyncram",
@@ -105,7 +105,7 @@ END SYN;
 -- Retrieval info: PRIVATE: JTAG_ENABLED NUMERIC "1"
 -- Retrieval info: PRIVATE: JTAG_ID STRING "INST"
 -- Retrieval info: PRIVATE: MAXIMUM_DEPTH NUMERIC "0"
--- Retrieval info: PRIVATE: MIFfilename STRING "../../Programs/C051-Ring_LED_Test/C051-Ring_LED_Test_ins.HEX"
+-- Retrieval info: PRIVATE: MIFfilename STRING "../../Programs/C052-Ring_LED_Loop_Test/C052-Ring_LED_Loop_Test_ins.HEX"
 -- Retrieval info: PRIVATE: NUMWORDS_A NUMERIC "512"
 -- Retrieval info: PRIVATE: RAM_BLOCK_TYPE NUMERIC "0"
 -- Retrieval info: PRIVATE: RegAddr NUMERIC "1"
@@ -120,7 +120,7 @@ END SYN;
 -- Retrieval info: CONSTANT: ADDRESS_ACLR_A STRING "NONE"
 -- Retrieval info: CONSTANT: CLOCK_ENABLE_INPUT_A STRING "NORMAL"
 -- Retrieval info: CONSTANT: CLOCK_ENABLE_OUTPUT_A STRING "BYPASS"
--- Retrieval info: CONSTANT: INIT_FILE STRING "../../Programs/C051-Ring_LED_Test/C051-Ring_LED_Test_ins.HEX"
+-- Retrieval info: CONSTANT: INIT_FILE STRING "../../Programs/C052-Ring_LED_Loop_Test/C052-Ring_LED_Loop_Test_ins.HEX"
 -- Retrieval info: CONSTANT: INTENDED_DEVICE_FAMILY STRING "Cyclone IV E"
 -- Retrieval info: CONSTANT: LPM_HINT STRING "ENABLE_RUNTIME_MOD=YES,INSTANCE_NAME=INST"
 -- Retrieval info: CONSTANT: LPM_TYPE STRING "altsyncram"
--- VHDL/Top_Level_Builds/R32V2020_Build_V001_A4_CE6/db/ip/sld4e341988/submodules/alt_sld_fab_alt_sld_fab_ident.sv
+++ VHDL/Top_Level_Builds/R32V2020_Build_V001_A4_CE6/db/ip/sld4e341988/submodules/alt_sld_fab_alt_sld_fab_ident.sv
@@ -20,7 +20,7 @@
 // Altera Identification hub
 //
 // Parameters
-//   DESIGN_HASH   : 2d2533ca13d96234a22c
+//   DESIGN_HASH   : a71703e21a75c5280e5e
 //   COUNT         : 1
 //   ROM_WIDTHS    : 4
 //   LATENCIES     : 0
@@ -39,7 +39,7 @@ module alt_sld_fab_alt_sld_fab_ident
 );
 
 
-wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h2d2533ca13d96234a22c };
+wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'ha71703e21a75c5280e5e };
 
 reg [3:0] result_0;
 always @(address_0 or data_0) begin
