bd8fb0c40c60c40636fc97625c959e4d7a757cea
Doug Gilliland <doug@douglasgilliland.com>
2019-06-11 18:25:55 -0400

    C054 - Use new mSec timer hardware - works

---
+++ VHDL/Programs/C054-Delay_mS_uS_Test/C054-Delay_mS_uS_Test.asm
@@ -0,0 +1,27 @@
+; delay_mS Test
+versionString:	.string "R32V2020 > "		; here to prevent an empty data file
+start:
+	lix	PAR,0x3000		; Seven Segment LED lines
+	ads	r8,r0,MINUS1	; Turn on all LED bits should sign extend to 32-bits
+	spl	r8				; Write out LED bits
+	lix	r8,500			; count for 500 mSec
+	bsr	delay_mS		; call delay_ms
+	lix	PAR,0x3000		; Seven Segment LED lines
+	ads	r8,ZERO,ZERO	; Turn off all LED bits
+	spl	r8				; Write out LED bits
+	lix	r8,500			; count for 500 mSec
+	bsr	delay_mS		; call delay_ms
+	bra	start
+; delay_mS - delay for the number of mSecs passed in r8
+; routine uses r8,r9
+delay_mS:
+	pss	r9
+	lix	PAR,0x3802		; address of the mSec counter
+	lpl	r9				; read the peripheral counter into r9
+	ads	r8,r9,r8		; terminal counter to wait until is in r8
+loop_delay_mS:
+	lpl	r9				; check the elapsed time counter
+	cmp	r8,r9
+	blt	loop_delay_mS
+	pus	r9
+	pus	r7
--- VHDL/Top_Level_Builds/R32V2020_Build_V001_A4_CE6/db/ip/sld4e341988/submodules/alt_sld_fab_alt_sld_fab_ident.sv
+++ VHDL/Top_Level_Builds/R32V2020_Build_V001_A4_CE6/db/ip/sld4e341988/submodules/alt_sld_fab_alt_sld_fab_ident.sv
@@ -20,7 +20,7 @@
 // Altera Identification hub
 //
 // Parameters
-//   DESIGN_HASH   : b29780a795c810769ba0
+//   DESIGN_HASH   : a080b080e36702a72c7a
 //   COUNT         : 1
 //   ROM_WIDTHS    : 4
 //   LATENCIES     : 0
@@ -39,7 +39,7 @@ module alt_sld_fab_alt_sld_fab_ident
 );
 
 
-wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'hb29780a795c810769ba0 };
+wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'ha080b080e36702a72c7a };
 
 reg [3:0] result_0;
 always @(address_0 or data_0) begin
