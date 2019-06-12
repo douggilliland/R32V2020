3c4b9b7a9d96c6dc0e6326fb6b17ed8c13672c62
Doug Gilliland <doug@douglasgilliland.com>
2019-06-12 14:57:10 -0400

    Compiled for A-C4E10 card

--- VHDL/Top_Level_Builds/R32V2020_Build_V001_A4_CE10/db/ip/sld4e341988/submodules/alt_sld_fab_alt_sld_fab_ident.sv
+++ VHDL/Top_Level_Builds/R32V2020_Build_V001_A4_CE10/db/ip/sld4e341988/submodules/alt_sld_fab_alt_sld_fab_ident.sv
@@ -20,7 +20,7 @@
 // Altera Identification hub
 //
 // Parameters
-//   DESIGN_HASH   : 69aa948e07b42c7cc596
+//   DESIGN_HASH   : 664e430e311858378bc0
 //   COUNT         : 1
 //   ROM_WIDTHS    : 4
 //   LATENCIES     : 0
@@ -39,7 +39,7 @@ module alt_sld_fab_alt_sld_fab_ident
 );
 
 
-wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h69aa948e07b42c7cc596 };
+wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h664e430e311858378bc0 };
 
 reg [3:0] result_0;
 always @(address_0 or data_0) begin
