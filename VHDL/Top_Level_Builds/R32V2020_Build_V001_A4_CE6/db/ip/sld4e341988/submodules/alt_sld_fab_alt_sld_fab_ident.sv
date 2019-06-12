5ef1784933165772a8ae9fd169a480292fb759d7
Doug Gilliland <doug@douglasgilliland.com>
2019-06-11 21:37:56 -0400

    C057 - scroll the screen (still need to fill bottom line with spaces)

--- VHDL/Top_Level_Builds/R32V2020_Build_V001_A4_CE6/db/ip/sld4e341988/submodules/alt_sld_fab_alt_sld_fab_ident.sv
+++ VHDL/Top_Level_Builds/R32V2020_Build_V001_A4_CE6/db/ip/sld4e341988/submodules/alt_sld_fab_alt_sld_fab_ident.sv
@@ -20,7 +20,7 @@
 // Altera Identification hub
 //
 // Parameters
-//   DESIGN_HASH   : 89a13dff7ffe8fea56f6
+//   DESIGN_HASH   : 898a3169be34fa1abaeb
 //   COUNT         : 1
 //   ROM_WIDTHS    : 4
 //   LATENCIES     : 0
@@ -39,7 +39,7 @@ module alt_sld_fab_alt_sld_fab_ident
 );
 
 
-wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h89a13dff7ffe8fea56f6 };
+wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h898a3169be34fa1abaeb };
 
 reg [3:0] result_0;
 always @(address_0 or data_0) begin
