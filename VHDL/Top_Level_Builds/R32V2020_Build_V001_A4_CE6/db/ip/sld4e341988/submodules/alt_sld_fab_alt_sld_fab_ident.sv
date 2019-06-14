7ccdb9c2f0bb818e275a768ce905aa1d308fa1bc
Doug Gilliland <doug@douglasgilliland.com>
2019-06-14 08:39:13 -0400

    No VHDL changes, tested backspace code

--- VHDL/Top_Level_Builds/R32V2020_Build_V001_A4_CE6/db/ip/sld4e341988/submodules/alt_sld_fab_alt_sld_fab_ident.sv
+++ VHDL/Top_Level_Builds/R32V2020_Build_V001_A4_CE6/db/ip/sld4e341988/submodules/alt_sld_fab_alt_sld_fab_ident.sv
@@ -20,7 +20,7 @@
 // Altera Identification hub
 //
 // Parameters
-//   DESIGN_HASH   : b92fe3d54e239e838b60
+//   DESIGN_HASH   : b80429fd81730b1a50d4
 //   COUNT         : 1
 //   ROM_WIDTHS    : 4
 //   LATENCIES     : 0
@@ -39,7 +39,7 @@ module alt_sld_fab_alt_sld_fab_ident
 );
 
 
-wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'hb92fe3d54e239e838b60 };
+wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'hb80429fd81730b1a50d4 };
 
 reg [3:0] result_0;
 always @(address_0 or data_0) begin
