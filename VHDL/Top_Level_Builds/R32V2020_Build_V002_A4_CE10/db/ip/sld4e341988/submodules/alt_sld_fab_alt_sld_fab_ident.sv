583219898324b107ffd673cd083094f40c90f82e
Doug Gilliland <doug@douglasgilliland.com>
2019-07-15 09:02:26 -0400

    New I2C example (work in progress)

--- VHDL/Top_Level_Builds/R32V2020_Build_V002_A4_CE10/db/ip/sld4e341988/submodules/alt_sld_fab_alt_sld_fab_ident.sv
+++ VHDL/Top_Level_Builds/R32V2020_Build_V002_A4_CE10/db/ip/sld4e341988/submodules/alt_sld_fab_alt_sld_fab_ident.sv
@@ -20,7 +20,7 @@
 // Altera Identification hub
 //
 // Parameters
-//   DESIGN_HASH   : 457c9d1d715190d78dbb
+//   DESIGN_HASH   : c3c83a60512910ed862a
 //   COUNT         : 1
 //   ROM_WIDTHS    : 4
 //   LATENCIES     : 0
@@ -39,7 +39,7 @@ module alt_sld_fab_alt_sld_fab_ident
 );
 
 
-wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h457c9d1d715190d78dbb };
+wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'hc3c83a60512910ed862a };
 
 reg [3:0] result_0;
 always @(address_0 or data_0) begin
