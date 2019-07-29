5f89a4b053663ff5aac38eadd2fe033932d42ee5
Doug Gilliland <doug@douglasgilliland.com>
2019-07-29 09:01:08 -0400

    Added a menu example

--- VHDL/Top_Level_Builds/R32V2020_Build_V002_A4_CE10/db/ip/sld4e341988/submodules/alt_sld_fab_alt_sld_fab_ident.sv
+++ VHDL/Top_Level_Builds/R32V2020_Build_V002_A4_CE10/db/ip/sld4e341988/submodules/alt_sld_fab_alt_sld_fab_ident.sv
@@ -20,7 +20,7 @@
 // Altera Identification hub
 //
 // Parameters
-//   DESIGN_HASH   : 2f4f9e5d5e0d20e15906
+//   DESIGN_HASH   : fdb4cf28610b32caef0a
 //   COUNT         : 1
 //   ROM_WIDTHS    : 4
 //   LATENCIES     : 0
@@ -39,7 +39,7 @@ module alt_sld_fab_alt_sld_fab_ident
 );
 
 
-wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h2f4f9e5d5e0d20e15906 };
+wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'hfdb4cf28610b32caef0a };
 
 reg [3:0] result_0;
 always @(address_0 or data_0) begin
