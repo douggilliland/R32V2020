62dd6e62bfa8ec970d9e957a7ec20de2b32e2414
Doug Gilliland <doug@douglasgilliland.com>
2019-08-29 07:43:34 -0400

    Built A4CE6 with keyboard wrapper

--- VHDL/Top_Level_Builds/R32V2020_Build_V002_A4_CE6/db/ip/sld4e341988/submodules/alt_sld_fab_alt_sld_fab_ident.sv
+++ VHDL/Top_Level_Builds/R32V2020_Build_V002_A4_CE6/db/ip/sld4e341988/submodules/alt_sld_fab_alt_sld_fab_ident.sv
@@ -20,7 +20,7 @@
 // Altera Identification hub
 //
 // Parameters
-//   DESIGN_HASH   : d6ffc633ee93087854a3
+//   DESIGN_HASH   : 1ed6757328507cf5357e
 //   COUNT         : 1
 //   ROM_WIDTHS    : 4
 //   LATENCIES     : 0
@@ -39,7 +39,7 @@ module alt_sld_fab_alt_sld_fab_ident
 );
 
 
-wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'hd6ffc633ee93087854a3 };
+wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h1ed6757328507cf5357e };
 
 reg [3:0] result_0;
 always @(address_0 or data_0) begin
